cbf3524e1cab5e945c0418363e2a477202161355
sebamuchut <sebamuchut@gmail.com>
2021-07-05 12:12:44 -0300

    05-07 mediodÃía

--- api/src/controllers/videogames.js
+++ api/src/controllers/videogames.js
@@ -1,11 +1,28 @@
 const {Videogame} = require('../db')
+const axios = require('axios')
+const { YOUR_API_KEY } = process.env;
+/*
+[ ] GET /videogames:
+Obtener un listado de los primeras 15 videojuegos
+Debe devolver solo los datos necesarios para la ruta principal
+[ ] GET /videogames?name="...":
+Obtener un listado de las primeros 15 videojuegos que contengan la palabra ingresada como query parameter
+Si no existe ningún videojuego mostrar un mensaje adecuado
+[ ] GET /videogame/{idVideogame}:
+Obtener el detalle de un videojuego en particular
+Debe traer solo los datos pedidos en la ruta de detalle de videojuego
+Incluir los géneros asociados */
 
-function Get_Games (req, res, next) {
-    Videogame.findAll()
+function Get_All_Games (req, res, next) {
+    if(req.query.name){
+        
+    }
+
+    return Videogame.findAll()
         .then((videogames) => res.json(videogames))
         .catch((error) => next(error))
 }
 
 module.exports = {
-    Get_Games
+    Get_All_Games
 }
--- api/src/routes/videogame_router.js
+++ api/src/routes/videogame_router.js
@@ -1,7 +1,18 @@
 const { Router } = require('express');
-const { Get_Games } = require('../controllers/videogames')
+const { Get_All_Games } = require('../controllers/videogames')
 const router = Router();
+/*[ ] GET /videogames:
+Obtener un listado de los primeras 15 videojuegos
+Debe devolver solo los datos necesarios para la ruta principal
+[ ] GET /videogames?name="...":
+Obtener un listado de las primeros 15 videojuegos que contengan la palabra ingresada como query parameter
+Si no existe ningún videojuego mostrar un mensaje adecuado
+[ ] GET /videogame/{idVideogame}:
+Obtener el detalle de un videojuego en particular
+Debe traer solo los datos pedidos en la ruta de detalle de videojuego
+Incluir los géneros asociados */
+router
+    .get('/', Get_All_Games)
 
-router.get('/', Get_Games)
 
 module.exports = router;
